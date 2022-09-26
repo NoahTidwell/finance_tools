8b4b3fea5703a0d0bf43b2a84d4f6726482ddd7a
Noah <ntidwell34@yahoo.com>
2022-09-26 15:40:44 -0500

    trade calculator that outputs the PnL of a trade

---
+++ tradeCalc.py
@@ -0,0 +1,13 @@
+buyPrice = float(input("At what price did you buy? >"))
+sellPrice = float(input("At what price did you sell? >"))
+
+loss = buyPrice - sellPrice
+decimalLoss = (loss / 100)
+f_userLoss = format(decimalLoss, ".2%")
+
+gainNeeded = (1 / (1 - decimalLoss)) - 1
+f_gainNeeded = format(gainNeeded, ".2%")
+
+
+print("You lost", f_userLoss, "in your trade.")
+print("You need to gain", f_gainNeeded, "to recapture your losses.")
