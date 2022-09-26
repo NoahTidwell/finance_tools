cab35a478b221bf2cc48f8121b76f670f88abda1
Noah <ntidwell34@yahoo.com>
2022-09-26 16:24:34 -0500

    update trade calculator to include profitable and breakeven trades

--- tradeCalc.py
+++ tradeCalc.py
@@ -1,13 +1,24 @@
 buyPrice = float(input("At what price did you buy? >"))
 sellPrice = float(input("At what price did you sell? >"))
 
-loss = buyPrice - sellPrice
-decimalLoss = (loss / buyPrice)
-f_userLoss = format(decimalLoss, ".2%")
+pnl = sellPrice - buyPrice
 
-gainNeeded = (1 / (1 - decimalLoss)) - 1
-f_gainNeeded = format(gainNeeded, ".2%")
+if pnl > 0:
+    decimalGain = (pnl / buyPrice)
+    f_gain = format(decimalGain, ".2%")
+    print("Congrats! You were profitable. You gained", f_gain)
+elif pnl == 0:
+    decimalGain = (pnl / buyPrice)
+    b_e = decimalGain * 100
+    f_be = format(b_e, ".2%")
+    print("You were breakeven on your trade", f_be)
+elif pnl < 0:
+    pnl = buyPrice - sellPrice
+    decimalLoss = (pnl / buyPrice)
+    f_userLoss = format(decimalLoss, ".2%")
 
+    gainNeeded = (1 / (1 - decimalLoss)) - 1
+    f_gainNeeded = format(gainNeeded, ".2%")
 
-print("You lost", f_userLoss, "in your trade.")
-print("You need to gain", f_gainNeeded, "to recapture your losses.")
+    print("You lost", f_userLoss, "in your trade.")
+    print("You need to gain", f_gainNeeded, "to recapture your losses.")
