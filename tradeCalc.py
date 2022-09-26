8e22409071ae9a73548c150748a45d6631e6be96
Noah <ntidwell34@yahoo.com>
2022-09-26 18:52:53 -0500

    color code trades

--- tradeCalc.py
+++ tradeCalc.py
@@ -6,12 +6,12 @@ pnl = sellPrice - buyPrice
 if pnl > 0:
     decimalGain = (pnl / buyPrice)
     f_gain = format(decimalGain, ".2%")
-    print("Congrats! You were profitable. You gained", f_gain)
+    print("Congrats! You were","\033[32m" ,"profitable.", "\033[0m", "You gained", "\033[32m",f_gain, "\033[0m")
 elif pnl == 0:
     decimalGain = (pnl / buyPrice)
     b_e = decimalGain * 100
     f_be = format(b_e, ".2%")
-    print("You were breakeven on your trade", f_be)
+    print("You were", "\033[33m","breakeven", "\033[0m","on your trade", "\033[33m", f_be, "\033[0m")
 elif pnl < 0:
     pnl = buyPrice - sellPrice
     decimalLoss = (pnl / buyPrice)
@@ -20,5 +20,5 @@ elif pnl < 0:
     gainNeeded = (1 / (1 - decimalLoss)) - 1
     f_gainNeeded = format(gainNeeded, ".2%")
 
-    print("You lost", f_userLoss, "in your trade.")
-    print("You need to gain", f_gainNeeded, "to recapture your losses.")
+    print("You", "\033[31m" "lost", f_userLoss, "\033[0m", "in your trade.")
+    print("You need to gain", "\033[32m", f_gainNeeded, "\033[0m", "to recapture your losses.")
